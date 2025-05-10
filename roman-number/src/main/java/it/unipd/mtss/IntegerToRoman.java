3dff60d797952dee4e3f81a6ec26cb362e97c4b9
Giacomo Giora <giacomo.giora.1@studenti.unipd.it>
2025-05-10 17:21:32 +0200

    Implementazione codice per passare i test sui primi 1000 numeri interi, close #14

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,11 +6,11 @@
 package it.unipd.mtss;
   
 public class IntegerToRoman {
-    private static final int[] values = {500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    private static final String[] symbols = {"D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     
     public static String convert(int number) {
-        if (number <= 0 || number > 500) {
+        if (number <= 0 || number > 1000) {
             return "";
         }
         StringBuilder roman = new StringBuilder();
