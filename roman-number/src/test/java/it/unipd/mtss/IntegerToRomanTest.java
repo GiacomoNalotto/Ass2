c41d8f61227331ab46048ab81c91bfd81d635c57
GiacomoNalotto <giacomo.nalotto03@gmail.com>
2025-05-10 17:02:41 +0200

    Implementazione test alcuni tra i primi 100 numeri interi #12

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -23,49 +23,55 @@ public class IntegerToRomanTest
     }
     
     @Test
-    public void intergerToRomanTest_50()
+    public void intergerToRomanTest_100()
     {   
         assertEquals("I", IntegerToRoman.convert(1));
-        assertEquals("II", IntegerToRoman.convert(2));
         assertEquals("IV", IntegerToRoman.convert(4));
         assertEquals("V", IntegerToRoman.convert(5));
-        assertEquals("VI", IntegerToRoman.convert(6));
         assertEquals("VIII", IntegerToRoman.convert(8));
         assertEquals("IX", IntegerToRoman.convert(9));
         assertEquals("X", IntegerToRoman.convert(10));
         assertEquals("XI", IntegerToRoman.convert(11));
-        assertEquals("XII", IntegerToRoman.convert(12));
         assertEquals("XV", IntegerToRoman.convert(15));
         assertEquals("XVII", IntegerToRoman.convert(17));
         assertEquals("XIX", IntegerToRoman.convert(19));
         assertEquals("XX", IntegerToRoman.convert(20));
         assertEquals("XXIV", IntegerToRoman.convert(24));
-        assertEquals("XXV", IntegerToRoman.convert(25));
-        assertEquals("XXIX", IntegerToRoman.convert(29));
         assertEquals("XXX", IntegerToRoman.convert(30));
         assertEquals("XXXV", IntegerToRoman.convert(35));
         assertEquals("XXXIX", IntegerToRoman.convert(39));
         assertEquals("XL", IntegerToRoman.convert(40));
         assertEquals("XLVII", IntegerToRoman.convert(47));
         assertEquals("L", IntegerToRoman.convert(50));
+        assertEquals("LIV", IntegerToRoman.convert(54));
+        assertEquals("LIX", IntegerToRoman.convert(59));
+        assertEquals("LX", IntegerToRoman.convert(60));
+        assertEquals("LXX", IntegerToRoman.convert(70));
+        assertEquals("LXXIII", IntegerToRoman.convert(73));
+        assertEquals("LXXX", IntegerToRoman.convert(80));
+        assertEquals("XC", IntegerToRoman.convert(90));
+        assertEquals("XCV", IntegerToRoman.convert(95));
+        assertEquals("XCIX", IntegerToRoman.convert(99));
+        assertEquals("C", IntegerToRoman.convert(100));
         
     }
 
 
     @Test
-    public void intergerToRomanBoundariesTest_50()
+    public void intergerToRomanBoundariesTest_100()
     {
         assertEquals("", IntegerToRoman.convert(-1));
         assertEquals("", IntegerToRoman.convert(0));
-        assertEquals("", IntegerToRoman.convert(51));
+        assertEquals("", IntegerToRoman.convert(101));
     }
     
-    public int romanToInteger_50(String romanNumeral) {
+    public int romanToInteger_100(String romanNumeral) {
         Map<Character, Integer> romanValues = new HashMap<>();
         romanValues.put('I', 1);
         romanValues.put('V', 5);
         romanValues.put('X', 10);
         romanValues.put('L', 50);
+        romanValues.put('C', 100);
 
         int result = 0;
         int prevValue = 0;
@@ -87,9 +93,9 @@ public class IntegerToRomanTest
     }
     
     @Test(timeout = 1000)
-    public void romanToIntegerTest_50(){
-        for(int i = 1; i <= 50; i++){
-            assertEquals(i, romanToInteger_50(IntegerToRoman.convert(i)));
+    public void romanToIntegerTest_100(){
+        for(int i = 1; i <= 100; i++){
+            assertEquals(i, romanToInteger_100(IntegerToRoman.convert(i)));
         }
     };
 }
